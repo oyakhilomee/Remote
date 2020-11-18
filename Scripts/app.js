c0aadfefe417b0ee1e7f8b987f2ddd7847279365
= <=>
2020-11-18 16:58:18 +0100

    added media query
    Added media queries to home section and started styling for about section

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed (".type", {
+   
+    strings: [
+    "Frontend Developer",
+    "Writer",
+    "Climate Activist",
+    ],
+    typeSpeed: 120,
+    backSpeed: 120,
+    loop: true,
+});
+
+
