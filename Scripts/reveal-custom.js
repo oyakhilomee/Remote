e354f21a40f65ff33180064b066b26f09f2bccaa
= <=>
2020-11-20 14:28:27 +0100

    testing

--- Scripts/reveal-custom.js
+++ Scripts/reveal-custom.js
@@ -1,1 +1,14 @@
-ScrollReveal().reveal('.right');
+const sr = ScrollReveal ({
+origin: 'top',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+
+sr.reveal('.img-home',{});
+
+sr.reveal('.hello',{});
+sr.reveal('.landing-h1',{delay:200});
+sr.reveal('.btn-view',{delay:400});
+sr.reveal('.social-ul',{delay:600});
