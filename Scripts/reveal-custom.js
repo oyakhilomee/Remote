bd2859a5681deef3599666d0c7b31592acc91e60
= <=>
2020-11-20 17:15:10 +0100

    test

--- Scripts/reveal-custom.js
+++ Scripts/reveal-custom.js
@@ -1,51 +1,85 @@
-const sr = ScrollReveal ({
-origin: 'top',
-distance: '80px',
-duration: 2000,
-reset: true
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '120px',
+    duration: 2000,
+    reset: true
 })
 
 // REVEAL for Landing
 
-sr.reveal('.img-home',{});
+sr.reveal('.img-home', {});
 
-sr.reveal('.hello',{});
-sr.reveal('.landing-h1',{delay:200});
-sr.reveal('.btn-view',{delay:400});
-sr.reveal('.social-ul',{delay:600});
+sr.reveal('.hello', {});
+sr.reveal('.landing-h1', {
+    delay: 200
+});
+sr.reveal('.btn-view', {
+    delay: 400
+});
+sr.reveal('.social-ul', {
+    delay: 600
+});
 
 // Scroll Reveal for About Section 
 
 
-sr.reveal('.version-control',{delay:200});
-sr.reveal('.strategy-div',{delay:400});
+sr.reveal('.version-control', {
+    delay: 200
+});
+sr.reveal('.strategy-div', {
+    delay: 400
+});
 
 
-sr.reveal('.information-architecture-img',{delay:200});
-sr.reveal('.information-architecture-div',{delay:400});
+sr.reveal('.information-architecture-img', {
+    delay: 200
+});
+sr.reveal('.information-architecture-div', {
+    delay: 400
+});
 
-sr.reveal('.design',{delay:200});
-sr.reveal('.design-div-class',{delay:300});
+sr.reveal('.design', {
+    delay: 200
+});
+sr.reveal('.design-div-class', {
+    delay: 300
+});
 
 // Scroll Reveal for Work Section
 
-sr.reveal('.work-pic-one-div',{delay:100});
+sr.reveal('.work-pic-one-div', {
+    delay: 100
+});
 
-sr.reveal('.work-pic-two-div',{delay:200});
+sr.reveal('.work-pic-two-div', {
+    delay: 200
+});
 
-sr.reveal('.work-pic-three-div',{delay:300});
+sr.reveal('.work-pic-three-div', {
+    delay: 300
+});
 
-sr.reveal('.work-pic-four-div',{delay:400});
+sr.reveal('.work-pic-four-div', {
+    delay: 400
+});
 
 
 // Scroll Reveal for Contact Section
 
 
-sr.reveal('.contact-h1',{delay:100});
+sr.reveal('.contact-h1', {
+    delay: 100
+});
 
 
-sr.reveal('.start-new-btn',{delay:200});
+sr.reveal('.start-new-btn', {
+    delay: 200
+});
 
-sr.reveal('.contact-ul',{delay:300});
+sr.reveal('.contact-ul', {
+    delay: 300
+});
 
-sr.reveal('.contact-img',{delay:300});
+sr.reveal('.contact-img', {
+    delay: 300
+});
