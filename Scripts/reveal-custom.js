d9335f12c42c23a2fd960fab2bdb67b6db53ae14
= <=>
2020-11-20 15:03:47 +0100

    added scrollreveal to all the sections

--- Scripts/reveal-custom.js
+++ Scripts/reveal-custom.js
@@ -5,6 +5,7 @@ duration: 2000,
 reset: true
 })
 
+// REVEAL for Landing
 
 sr.reveal('.img-home',{});
 
@@ -12,3 +13,39 @@ sr.reveal('.hello',{});
 sr.reveal('.landing-h1',{delay:200});
 sr.reveal('.btn-view',{delay:400});
 sr.reveal('.social-ul',{delay:600});
+
+// Scroll Reveal for About Section 
+
+
+sr.reveal('.version-control',{delay:200});
+sr.reveal('.strategy-div',{delay:400});
+
+
+sr.reveal('.information-architecture-img',{delay:200});
+sr.reveal('.information-architecture-div',{delay:400});
+
+sr.reveal('.design',{delay:200});
+sr.reveal('.design-div-class',{delay:300});
+
+// Scroll Reveal for Work Section
+
+sr.reveal('.work-pic-one-div',{delay:200});
+
+sr.reveal('.work-pic-two-div',{delay:400});
+
+sr.reveal('.work-pic-three-div',{delay:600});
+
+sr.reveal('.work-pic-four-div',{delay:800});
+
+
+// Scroll Reveal for Contact Section
+
+
+sr.reveal('.contact-h1',{delay:100});
+
+
+sr.reveal('.start-new-btn',{delay:200});
+
+sr.reveal('.contact-ul',{delay:300});
+
+sr.reveal('.contact-img',{delay:300});
